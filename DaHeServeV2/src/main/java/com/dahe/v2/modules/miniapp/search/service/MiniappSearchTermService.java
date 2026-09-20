a474198268752243232ee4fdf832e7e931a75538
19continue <131459414+19continue@users.noreply.github.com>
2026-09-20 15:58:29 +0800

    Delete DaHeServeV2 directory

--- DaHeServeV2/src/main/java/com/dahe/v2/modules/miniapp/search/service/MiniappSearchTermService.java
+++
@@ -1,7 +0,0 @@
-package com.dahe.v2.modules.miniapp.search.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.dahe.v2.modules.miniapp.search.model.MiniappSearchTerm;
-
-public interface MiniappSearchTermService extends IService<MiniappSearchTerm> {
-}
